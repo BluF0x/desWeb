5bd9d92484ea18acdfce57fe5f5fe1fc3e16b1f6
BluF0x <josue.elias.f@gmail.com>
2023-09-12 12:20:59 -0300

    Final

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,15 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.get('/cool', (req, res)=>{
+  res.send("Coooool")
+})
+
+router.get('/cool/beach', (req, res)=>{
+  res.send("De boa na praia")
+})
+router.get('/ifc', (req, res)=>{
+  res.send("IFC Sombrio > IFC Santa Rosa")
+})
+
 module.exports = router;

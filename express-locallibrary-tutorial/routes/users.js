15024322a8c9dd4dbfa53ef63f6acc83c9d69b47
BluF0x <josue.elias.f@gmail.com>
2023-09-12 09:07:01 -0300

    commit

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/cool', function(req, res, next) {
-  res.send('Holy cow');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
